077b06b34ff83ab8c075f733803f201187f2d330
rita612 <rita.xavier.lima@escola.pr.gov.br>
2024-11-11 08:56:06 -0300

    cd3

--- script.js
+++ script.js
@@ -8,63 +8,60 @@ let xRaquete = 10, yRaquete = 150, velRaquete = 10;
 let xRaqueteOponente = 510, yRaqueteOponente = 150, velOponente = 5;
 
 function setup() {
-createCanvas(520, 520);
+  createCanvas(520, 520);
 }
 
 function draw() {
-background(0);
-desenhaBolinha();
-movimentaBolinha();
-verificaColisaoBorda();
-
-desenhaRaquete(xRaquete, yRaquete);
-desenhaRaquete(xRaqueteOponente, yRaqueteOponente);
-
-movimentaMinhaRaquete();
-movimentaRaqueteOponente();
-
-verificaColisaoRaquete(xRaquete, yRaquete);
-verificaColisaoRaquete(xRaqueteOponente, yRaqueteOponente);
+  background(0);
+  desenhaBolinha();
+  movimentaBolinha();
+  verificaColisaoBorda();
+  
+  desenhaRaquete(xRaquete, yRaquete);
+  desenhaRaquete(xRaqueteOponente, yRaqueteOponente);
+  
+  movimentaMinhaRaquete();
+  movimentaRaqueteOponente();
+  
+  verificaColisaoRaquete(xRaquete, yRaquete);
+  verificaColisaoRaquete(xRaqueteOponente, yRaqueteOponente);
 }
 
 function desenhaBolinha() {
-circle(xBolinha, yBolinha, diametro);
+  circle(xBolinha, yBolinha, diametro);
 }
 
 function movimentaBolinha() {
-xBolinha += velXBolinha;
-yBolinha += velYBolinha;
+  xBolinha += velXBolinha;
+  yBolinha += velYBolinha;
 }
 
 function verificaColisaoBorda() {
-if (xBolinha + raio > width || xBolinha
-raio < 0) velXBolinha *= -1;
-if (yBolinha + raio > height || yBolinha
-raio < 0) velYBolinha *= -1;
+  if (xBolinha + raio > width || xBolinha - raio < 0) velXBolinha *= -1;
+  if (yBolinha + raio > height || yBolinha - raio < 0) velYBolinha *= -1;
 }
+
 function desenhaRaquete(x, y) {
-rect(x, y, raqueteComprimento, raqueteAltura);
+  rect(x, y, raqueteComprimento, raqueteAltura);
 }
 
 function movimentaMinhaRaquete() {
-if (keyIsDown(UP_ARROW)) yRaquete -= velRaquete;
-if (keyIsDown(DOWN_ARROW)) yRaquete += velRaquete;
+  if (keyIsDown(UP_ARROW)) yRaquete -= velRaquete;
+  if (keyIsDown(DOWN_ARROW)) yRaquete += velRaquete;
 }
 
 function movimentaRaqueteOponente() {
-if (yBolinha > yRaqueteOponente + raqueteAltura / 2) yRaqueteOponente += velOponente;
-if (yBolinha < yRaqueteOponente + raqueteAltura / 2) yRaqueteOponente -= velOponente;
+  if (yBolinha > yRaqueteOponente + raqueteAltura / 2) yRaqueteOponente += velOponente;
+  if (yBolinha < yRaqueteOponente + raqueteAltura / 2) yRaqueteOponente -= velOponente;
 }
 
 function verificaColisaoRaquete(x, y) {
-if (
-xBolinha
-raio < x + raqueteComprimento &&
-xBolinha + raio > x &&
-yBolinha + raio > y &&
-yBolinha
-raio < y + raqueteAltura
-) {
-velXBolinha *= -1;
-}
+  if (
+    xBolinha - raio < x + raqueteComprimento &&
+    xBolinha + raio > x &&
+    yBolinha + raio > y &&
+    yBolinha - raio < y + raqueteAltura
+  ) {
+    velXBolinha *= -1;
+  }
 }
